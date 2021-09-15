3f4ab7ee30495588c51cd18ba5fb750b60327332
Martin <martinknu22@gmail.com>
2021-09-15 15:45:49 +0200

    first commt

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+	<article>
+
+		<h1><a href="post">My First Post</a></h1>
+
+		<p>
+			This is my first post
+		</p>
+
+
+	</article>
+
+	<article>
+
+		<h1><a href="post">My Second Post</a></h1>
+
+		<p>
+			This is my first post
+		</p>
+
+
+	</article>
+
+	<article>
+
+		<h1><a href="post">My Third Post</a></h1>
+
+		<p>
+			This is my first post
+		</p>
+
+
+	</article>
+</body>
